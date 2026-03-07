1c5b0ec93d93758e2176176eb8543c079c12e821
YannMassoumou <YannPC>
2026-03-07 15:56:55 +0100

    completed the sign/register modal
    ?

--- src/app/shared/components/header-button-modal/header-button-modal.ts
+++ src/app/shared/components/header-button-modal/header-button-modal.ts
@@ -1,10 +1,27 @@
-import { NgSwitch } from '@angular/common';
+import { NgIf } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-header-button-modal',
-  imports: [FormsModule, NgSwitch],
+  imports: [
+    NgIf,
+    FormsModule,
+    MatCardModule,
+    MatTabsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatIconModule,
+  ],
   standalone: true,
 
   templateUrl: './header-button-modal.html',
@@ -13,17 +30,14 @@ import { FormsModule } from '@angular/forms';
 export class HeaderButtonModal {
   /** 'sign-in' | 'register' (could also be an enum) */
   @Input() mode!: 'sign-in' | 'register';
-
-  // control visibility yourself or via a service
+  //control visibility yourself or via a service
   visible = false;
-
   open() {
     this.visible = true;
   }
   close() {
     this.visible = false;
   }
-
   onSubmit(formValues: any) {
     if (this.mode === 'sign-in') {
       // call authService.signIn(...)
